5176e5cf715b3837c72f98c4e25609ac83a8c0cf
yeyuan98 <yuanye1998@gmail.com>
2024-11-05 14:01:40 -0500

    New: `str_split_summary`

--- R/filter.R
+++ R/filter.R
@@ -19,3 +19,30 @@ mR_filter <- function(mRcrd, what){
 
   return(mRcrd[which])
 }
+
+#' Applying summary function to string splits
+#'
+#' @param string Input vector, passed to [stringr::str_split()].
+#' @param pattern Pattern to look for, passed to [stringr::str_split()].
+#' @param summary Summary function that will be applied to each split.
+#'
+#' @return Vector same length as input vector
+#' @export
+#'
+#' @examples
+#' t <- c("0.1,0.3,0.7", "NA", "0.2", "")
+#' str_split_summary(t, ",", \(x) mean(as.numeric(x)))
+str_split_summary <- function(string, pattern, summary){
+
+  # Split string
+  splits <- stringr::str_split(string, pattern)
+
+  # Apply summary function to get length=1 outputs (check correctness)
+  splits <- lapply(splits, summary)
+  lengths <- lapply(splits, length) |> unlist()
+  if (any(lengths != 1))
+    rlang::abort("Summary function must return length()=1.")
+
+  # Unlist and return
+  return(unlist(splits))
+}
